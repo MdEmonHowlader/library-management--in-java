44d4577eb7b4baa1c9c7f2528a0a23eeead3efa6
Emon <emon22205101732@diu.edu.bd>
2023-10-29 12:38:16 +0600

    update

--- Library/Databace.java
+++ Library/Databace.java
@@ -1,5 +1,12 @@
 package Library;
-
+import java.util.*;  
 public class Databace {
+    ArrayList<User> users=new ArrayList<User>();
+    ArrayList<String>  username= new ArrayList<String>();
+    public void AddUser(User e){
+        users.add(e);
+        username.add(e.getName());
+    }
     
+
 }
