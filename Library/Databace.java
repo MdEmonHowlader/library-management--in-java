e86194dbf38f32b41e6995f70369686b6bf2e8c3
Emon <emon22205101732@diu.edu.bd>
2023-11-16 22:14:47 +0600

    update

--- Library/Databace.java
+++ Library/Databace.java
@@ -1,8 +1,25 @@
 package Library;
+
+import java.io.File;
 import java.util.*;  
 public class Databace {
-    ArrayList<User> users=new ArrayList<User>();
-    ArrayList<String> username= new ArrayList<String>();
+   private ArrayList<User> users=new ArrayList<User>();
+   private ArrayList<String> username= new ArrayList<String>();
+   private ArrayList<Book> books= new ArrayList<Book>();
+   private ArrayList<String> bookname= new ArrayList<String>();
+
+   private File userfile =new File(Main.class.getClassLoader().getResource("\\Users").getFile());
+    private File booksfile =new File(Main.class.getClassLoader().getResource("\\Books").getFile());
+
+    public Databace(){
+        if(!userfile.exists()){
+            userfile.mkdirs();
+        }
+          if(!booksfile.exists()){
+            booksfile.mkdirs();
+        }
+
+    }
     public void AddUser(User e){
         users.add(e);
         username.add(e.getName());
@@ -19,6 +36,12 @@ public class Databace {
     }
     public User getUser(int n){
         return users.get(n);
+    }
+
+    public void AddBooks(Book book){
+        books.add(book);
+        bookname.add(book.getNmae());
+
     }
 
 }
